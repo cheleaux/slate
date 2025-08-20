46adcd5290ef8b048093a45dec14f8eab62b30e5
cheleaux <nazareth.dnd@gmail.com>
2025-08-20 19:22:52 +0100

    Âvalidation for maxAge input in token generator to accomedate added expire duration inputs

--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     auth: {
         jwt: {
-            tokenExpiryTime: '15m' // 15 minutes...
+            tokenExpiryTime: '15m', // 15 minutes...
+            relativeTimeNotationFormat: /^[1-9]\d*(s|m|h|d|w)$/,
         }
     }
 }
