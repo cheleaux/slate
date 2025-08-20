9c3f68906e80827e7d2bb2b92e06246b2e8cc95b
cheleaux <nazareth.dnd@gmail.com>
2025-08-20 19:22:12 +0100

    Âvalidation for maxAge input in token generator to accomedate added expire duration inputs

---
+++ server/src/utils/helpers.js
@@ -0,0 +1,10 @@
+const AuthConfig = require('../config.js').auth
+
+function isRelativeTimeNotation(string){
+    if(typeof string !== 'string') return false;
+    // Validate given string as a duration shorthand using the regex format in config
+    const timeNotationFormat = AuthConfig.jwt.relativeTimeNotationFormat
+    return timeNotationFormat.test(string)
+}
+
+module.export = { isRelativeTimeNotation }
