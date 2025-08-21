d2aea3a368f892ba73b27c255db095cbf6c33a62
cheleaux <nazareth.dnd@gmail.com>
2025-08-21 21:03:14 +0100

    added blacklist check to token authentication middleware

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,18 +1,26 @@
 const { verifyJWTToken } = require('../utils/token.js')
+const { BlacklistedToken } = require('../auth/tokenBlacklist.js')
+
 
 function authenticationRequired(req, res, next){
-    const token = req.body.token
+    let token = req.headers.authorization || req.body.token
+    token = token.startsWith('Bearer') ? token.slice(6).trimStart() : token ;
     // verify the request token and set the user with the decoded data from token 
     verifyJWTToken(token)
         .then((decodedToken) => {
-            req.user = decodedToken.data
-            next()
+            BlacklistedToken.findOne({tokenId: decodedToken._id}).then((token) => {
+                if(!token){
+                    req.user = decodedToken.data
+                    return next()
+                }
+                console.warn("An authenticated request was attempted with a blacklisted token!\n", `BlackedToken reference: ${ token._id }`)
+                res.status(403).json({message: "This token is invalid"})
+            })
         })
         .catch((err) => {
             console.error("Authentication Failed -", err.message)
             res.status(400).json({message: "Unable to authenticate user", error: err.message})
         })
 }
-
 
 module.exports = { authenticationRequired }
