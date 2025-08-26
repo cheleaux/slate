4c89f1cb7dee4c9f3982ed05d011eced1f881cbd
cheleaux <nazareth.dnd@gmail.com>
2025-08-26 13:29:52 +0100

    created digital asset persistence class

---
+++ server/src/graphicAssets/digitalAssetPersistence.js
@@ -0,0 +1,32 @@
+const { Image } = require('./models/image.js')
+
+
+class DigitalAssetStore {
+    static async saveImage(imageDetails){
+        const { ownerId, fileName, fileSize, fileKey } = imageDetails
+        const image = new Image({ ownerId, fileName, fileSize, fileKey })
+        return await image.save()
+    }
+
+
+    static async updateImage(newImageDetails, id){
+        const image = await Image.findOne(id)
+        for(const key in newImageDetails){
+            if(!Object.hasOwn(image, key)){
+                throw new Error("Invalid Image property")
+            }
+            image[key] = newImageDetails[key]
+        }
+        return await image.save()
+    }
+
+    static async deleteImage(id, userId){
+        const image = await Image.findOne(id)
+        if(image.userId !== userId){
+            throw new Error("requesting user id must match the owner id")
+        }
+        return await Image.deleteOne({_id: id})
+    }
+}
+
+module.exports = DigitalAssetStore
--- server/src/graphicAssets/models/image.js
+++ server/src/graphicAssets/models/image.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const ImageSchema = mongoose.Schema({
-    userId: {
+    ownerId: {
         type: mongoose.Types.ObjectId,
         required: true
     },
