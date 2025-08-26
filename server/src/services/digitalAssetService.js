9c76e746153e30679ba494c4f6cff88b74078ea3
cheleaux <nazareth.dnd@gmail.com>
2025-08-26 18:29:51 +0100

    created digital asset service with basic image asset crud func

---
+++ server/src/services/digitalAssetService.js
@@ -0,0 +1,36 @@
+const digitalAssetStore = require('../graphicAssets/digitalAssetPersistence.js')
+
+class DigitalAssetService {
+    static #assetStore = digitalAssetStore
+
+    static async createImage(userId, imageDetails){
+        try {
+            // Set the owner id to the current user as procedure requirement
+            if(!userId){
+                throw new Error("User Id is required to create image")
+            }
+            imageDetails.ownerId = userId
+            // Create the unique fileKey with uuid attached and persist image data
+            const fileKey = `${uuid.v4()}-${imageDetails.fileName}`
+            imageDetails.fileKey = fileKey
+            return await this.#assetStore.saveImage(imageDetails)
+        } catch(err) {
+            console.error("Failed to create asset 'image'\n", err)
+            throw err
+        }
+    }
+
+    static async deleteImage(userId, imageId){
+        try {
+            if(!userId){
+                throw new Error("User Id is required to create image")
+            }
+            return await this.#assetStore.deleteImage(imageId, userId)
+        } catch(err) {
+            console.error("Failed to delete asset 'image'")
+            throw err
+        }
+    }
+}
+
+module.exports = DigitalAssetService
